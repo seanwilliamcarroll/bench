7d27edc555ca20d7e9ce887d02efe19c1c0a4499
Sean Carroll <seanwilliamcarroll@gmail.com>
2026-02-28 16:27:58 -0500

    Work towards reading the regs myself

--- src/record.cpp
+++ src/record.cpp
@@ -1,10 +1,16 @@
 #include "record.hpp"
-#include <_time.h>
+#include <asm/ptrace.h>
 #include <chrono>
+#include <elf.h>
 #include <iostream>
+#include <signal.h>
+#include <sys/ptrace.h>
 #include <sys/resource.h>
+#include <sys/signal.h>
+#include <sys/uio.h>
 #include <sys/wait.h>
 #include <thread>
+#include <time.h>
 #include <unistd.h>
 
 timespec subtract(timespec start_time, timespec end_time) {
@@ -30,6 +36,11 @@ void print_rusage(const struct rusage &ru) {
   std::cout << "involuntary context switches: " << ru.ru_nivcsw << "\n";
 }
 
+void wait_n_msec(int n) {
+  timespec interval = {0, 1'000'000 * n};
+  nanosleep(&interval, nullptr);
+}
+
 int fork_exec(char *argv[]) {
   timespec start_time;
   clock_gettime(CLOCK_MONOTONIC, &start_time);
@@ -40,9 +51,38 @@ int fork_exec(char *argv[]) {
     return -1;
   }
   if (pid == 0) {
+    ptrace(PTRACE_TRACEME, 0, 0, 0);
     return execvp(argv[0], argv);
   }
-  wait(NULL);
+  std::cout << "Parent of pid: " << pid << std::endl;
+
+  int status;
+  waitpid(pid, &status, 0);
+
+  // Want to poll basically
+  while (true) {
+    ptrace(PTRACE_CONT, pid, 0, 0);
+    wait_n_msec(10);
+    if (kill(pid, SIGSTOP) < 0) {
+      std::cout << "child gone" << std::endl;
+      break;
+    }
+    waitpid(pid, &status, 0);
+    if (WIFEXITED(status)) {
+      std::cout << "Exited" << std::endl;
+      break;
+    } else if (WIFSTOPPED(status)) {
+      user_pt_regs regs;
+      iovec iov = {&regs, sizeof(regs)};
+      ptrace(PTRACE_GETREGSET, pid, (void *)NT_PRSTATUS, &iov);
+      std::cout << "Did reg read!" << std::endl;
+    } else {
+      std::cout << "Can this happen?" << std::endl;
+      break;
+    }
+  }
+
+  // wait(NULL);
   timespec end_time;
   clock_gettime(CLOCK_MONOTONIC, &end_time);
   auto difference = subtract(start_time, end_time);
